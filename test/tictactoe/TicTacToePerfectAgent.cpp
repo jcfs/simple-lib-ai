10580ffa969c5dbc634d8cd2af2c6ec4b419df79
Joao Salavisa <joao.salavisa@gmail.com>
2016-06-22 03:21:47 +0100

    perfect agent

---
+++ test/tictactoe/TicTacToePerfectAgent.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+#include "TicTacToePerfectAgent.h"
+#include "TicTacToeValidator.h"
+
+int TicTacToePerfectAgent::play(char * game, int player) {
+
+  int move = -1;
+  int score = -2;
+
+  if (game[4] == BLANK) return 4;
+
+  int c = 0;
+
+  for(int i = 0; i < 9; i++) {
+    if (game[i] != BLANK)
+      c++;
+  }
+
+  for(int i = 0; i < 9; ++i) {
+    if(game[i] == 0) {
+      game[i] = 1;
+      int tempScore = -minmax(game, -1);
+      game[i] = 0;
+      if(tempScore > score) {
+        score = tempScore;
+        move = i;
+      }
+    }
+  }
+
+  return move;
+}
+
+int TicTacToePerfectAgent::minmax(char * game, int player) {
+    //How is the position like for player (their turn) on game?
+    int winner = TicTacToeValidator::validate(game);
+    if(winner != 0 && winner != -3) return winner*player;
+
+    int move = -1;
+    int score = -2;//Losing moves are preferred to no move
+    int i;
+    for(i = 0; i < 9; ++i) {//For all moves,
+        if(game[i] == 0) {//If legal,
+            game[i] = player;//Try the move
+            int thisScore = -minmax(game, player*-1);
+            if(thisScore > score) {
+                score = thisScore;
+                move = i;
+            }//Pick the one that's worst for the opponent
+            game[i] = 0;//Reset game after try
+        }
+    }
+    if(move == -1) return 0;
+    return score;
+}
