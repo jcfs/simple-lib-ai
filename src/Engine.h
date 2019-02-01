c3d590fadb10fb237ed19c064b6cf61734e12b18
Joao Salavisa <jsa45@ukc02vr2swhtdd.bskyb.com>
2019-02-01 17:53:49 +0000

    update license

--- src/Engine.h
+++ src/Engine.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2016, 2017, 2018 Joao Salavisa <joao.salavisa@gmail.com>.
+    Copyright (c) 2016, 2017, 2018, 2019 Joao Salavisa <joao.salavisa@gmail.com>.
 
     Permission is hereby granted, free of charge, to any person obtaining
     a copy of this software and associated documentation files (the "Software"),
